90aadf59efb14de96ce1052c647f6ca4d6151faa
Rehagana16 <rehaganak@gmail.com>
2020-11-13 11:29:25 +0700

    initial commit(fungsi stem)

---
+++ src/search-engine.py
@@ -0,0 +1,15 @@
+from nltk.stem import PorterStemmer 
+from nltk.tokenize import word_tokenize 
+
+def stemming(sentence):
+   
+    ps = PorterStemmer() 
+    words = word_tokenize(sentence) 
+    hasil_stem = []
+    for w in words: 
+        hasil_stem.append(ps.stem(w))
+    return hasil_stem
+
+kata = "Programmers program with programming languanges"
+hasil = stemming(kata)
+print(hasil)
