cbd254b051153a97452e6e58ec5319357d6dd725
Reinaldo16519320 <sandyshroud00@gmail.com>
2020-11-14 10:34:15 +0800

    upload file

--- src/app.py
+++ src/app.py
@@ -1,10 +1,34 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask,render_template,request  
+from flask import Flask,render_template,request
+import os  
 import search_engine                                     
 app = Flask(__name__, template_folder='templates') 
 
-@app.route('/',methods=["GET","POST"])   # link 127.0.0.1:5000/ 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/")
+def index():
+	return render_template("upload.html")
+
+@app.route("/upload", methods= ['POST'])
+def upload() :
+	target = os.path.join(APP_ROOT, 'pdf_database/')
+	print(target)
+
+	if not os.path.isdir(target) :
+		os.mkdir(target)
+	
+	for file in request.files.getlist("file") :
+		print(file)
+		filename = file.filename
+		destination = "/".join([target, filename])
+		print(destination)
+		file.save(destination)
+		
+	return render_template("upload_complete.html")
+
+@app.route('/search',methods=["GET","POST"])   # link 127.0.0.1:5000/ 
 def search() : 
 	result=[]   
 	word =""
@@ -13,7 +37,9 @@ def search() :
          result = search_engine.stemming(word) 
          if result:
 	         pass
-	return render_template("search.html",result=result,word=word)  
+	return render_template("search.html",result=result,word=word)
+
 
 if __name__ == '__main__':
    app.run(debug=True)
+
