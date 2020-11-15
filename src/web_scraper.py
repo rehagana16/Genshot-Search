11d59dd414674f1a30e029a4b230d99f6abe664f
Reinaldo16519320 <sandyshroud00@gmail.com>
2020-11-15 22:32:37 +0800

    merge

--- src/web_scraper.py
+++ src/web_scraper.py
@@ -13,19 +13,20 @@ character_description = article.find('blockquote', class_='pull-quote').p.text
 article_title = soup.find('aside', class_= 'portable-infobox pi-background pi-europa pi-theme-char pi-layout-default')
 character_name = article_title.h2.text
 
-#article_vision = soup.find('td', class_= 'pi-horizontal-group-item pi-data-value pi-font pi-border-color pi-item-spacing')
-#character_vision = article_title.find('div', class_= 'pi-data-value pi-font').a.text
+article_vision = soup.find('td', class_= 'pi-horizontal-group-item pi-data-value pi-font pi-border-color pi-item-spacing')
+character_vision = article_title.find('div', class_= 'pi-data-value pi-font').a.text
 
 
 print(character_name)
 print(character_description)
 
-character_name += str('.txt')
-print(character_name)
+#character_name += str('.txt')
+#print(character_name)
 
-fpath = os.path.join('txt_database', character_name)
-f = codecs.open(fpath,"w", encoding = 'utf-8')
-f.write(character_description)
-f.close()
+#buat save hasil scraping ke file .txt
+#fpath = os.path.join('txt_database', character_name)
+#f = codecs.open(fpath,"w", encoding = 'utf-8')
+#f.write(character_description)
+#f.close()
 
 
