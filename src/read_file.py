a1bf2954c86a243b1010fc9fb9b094fcf5da25e4
Reinaldo16519320 <sandyshroud00@gmail.com>
2020-11-16 09:16:55 +0800

    merge

--- src/read_file.py
+++ src/read_file.py
@@ -1,14 +1,6 @@
 import os
 import codecs
 
-def text_to_list_of_string(filename):
-    fpath = os.path.join('txt_database', filename)
-    file = open(fpath,"r")
-    content = file.read()
-    content_list = content.split(".")
-    file.close()
-    return(content_list)
-
 def text_read(filename):
     fpath = os.path.join('txt_database', filename)
     file = codecs.open(fpath,"r", encoding = 'utf-8')
